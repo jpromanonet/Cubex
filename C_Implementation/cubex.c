b56853a31cfcc52c63bb7ec342ff8ac7f7fad32c
jpromanonet <programming@jpromano.net>
2020-06-28 14:35:09 -0300

    Seeding the VM

--- C_Implementation/cubex.c
+++ C_Implementation/cubex.c
@@ -1,7 +1,15 @@
 #include "common.h"
 
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
+
 int main(int argc, const char *argv[])
 {
     return 0;
-    printf("TEST");
 }
